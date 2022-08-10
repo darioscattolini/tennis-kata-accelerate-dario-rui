d630405349bdbf0eb2d69826ea2f91b42e843ad3
Rui Sousa <ruisousa05@gmail.com>
2022-08-10 14:45:10 -0100

    create instance of MarsRover

---
+++ app/src/main/kotlin/tennis/kata/accelerate/MarsRover.kt
@@ -0,0 +1,5 @@
+package tennis.kata.accelerate
+
+class MarsRover {
+
+}
--- app/src/test/kotlin/tennis/kata/accelerate/MarsRoverTest.kt
+++ app/src/test/kotlin/tennis/kata/accelerate/MarsRoverTest.kt
@@ -1,5 +1,12 @@
 package tennis.kata.accelerate
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class MarsRoverTest {
 
+    @Test
+    fun createInstanceOfMarsRover() {
+        assertThat(MarsRover()).isNotNull()
+    }
 }
