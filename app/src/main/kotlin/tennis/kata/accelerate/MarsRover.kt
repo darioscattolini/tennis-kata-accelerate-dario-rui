eee10ee2aea2934fc819205f855b938ee0e73982
Rui Sousa <ruisousa05@gmail.com>
2022-08-10 15:51:21 -0100

    remove function body in function move forward

--- app/src/main/kotlin/tennis/kata/accelerate/MarsRover.kt
+++ app/src/main/kotlin/tennis/kata/accelerate/MarsRover.kt
@@ -3,7 +3,5 @@ package tennis.kata.accelerate
 class MarsRover(x: Int = 0, y: Int = 0, val direction: String = "N") {
     val position = Pair(x, y)
 
-    fun moveForward(): Pair<Int, Int> {
-        return Pair(3,0)
-    }
+    fun moveForward() = Pair(3,0)
 }
